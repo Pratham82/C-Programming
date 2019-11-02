28c5845422da72d4f7f618f89420fd20da318c77
Prathamesh Mali <mali.prathamesh82@gmil.com>
2019-11-02 16:26:36 +0530

    Code for variables and data types

---
+++ Variables & data types.c
@@ -0,0 +1,22 @@
+//Variables and data types
+
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    float y;
+    double z;
+    char ch;
+    _Bool bool1;
+
+    x= 20;
+    y= 144.55;
+    z= 49.666666666;
+    ch= 'C';
+    bool1= 0 ;
+
+    printf("value of x,y,z,ch, bool is: %d, %f, %e,%c,%i ",  x, y,z,ch,bool1);
+
+    return 0;
+}
