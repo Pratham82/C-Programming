923b84ab882ca1148cdb28cece6137126bc487a7
Prathamesh Mali <mali.prathamesh82@gmil.com>
2019-11-02 16:19:24 +0530

    Code for takig input

---
+++ Taking Input.c
@@ -0,0 +1,16 @@
+//Taking input from the user/
+
+#include <stdio.h>
+
+int main()
+{
+    int i;
+
+    printf("Enter the number here: ");
+    scanf("%d", &i);
+
+    printf("\nYour entered number is: %d", i);
+
+    return 0;
+
+}
